9a013e1f288b62a2fdc830ac8f4a410a077cda2f
vause@yahoo.com <vause@yahoo.com>
2017-11-27 18:12:56 -0600

    Made list of redditisms
    Redditisms to pull for each comment. Also need karma

--- Comment_Extraction - Count words.py
+++ Comment_Extraction - Count words.py
@@ -1,15 +1,15 @@
 import praw
 from collections import Counter
-
+list = ['ftfy', 'imo', 'DAE', 'ELI5', 'imho', 'TIL', 'IIRC', 'ITT', 'TLDR', 'TL;DR', 'MIC', 'MFW', 'SMH', 'MRW', 'AFAIK', 'AMA', 'FFS', 'NFSW', 'NSFL', 'TIFU', 'OP', '/s', 'SJW', 'Neckbeard', 'Karmawhore', 'Shitpost', 'Circlejerk' 'rip my inbox']
 reddit = praw.Reddit(client_id='BvQLViitNTyU6A',
                      client_secret='IgYF_uDdUYH2Jl8tpY9UFuCf1yY',
                      password='Password1',
 					 user_agent='This will be a script',
                      username='awaythrowcs')
 print (reddit.user.me())
-submission = reddit.submission(url='https://www.reddit.com/r/cscareerquestions/comments/7cr3tf/should_i_continue_with_a_cs_major/')
+submission = reddit.submission(url='https://www.reddit.com/r/Jokes/comments/7fukkv/i_wish_my_college_was_run_by_ea/')
 submission.comments.replace_more(limit=0)
 for top_level_comment in submission.comments:
-    print(top_level_comment.body)
-    help = Counter(top_level_comment.body.split()).most_common()
-    print(help[:10])
+    if any(word in top_level_comment.body for word in list):
+        help = Counter(top_level_comment.body.split()).most_common()
+        print(help[:10])
