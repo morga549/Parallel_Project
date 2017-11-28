ae8f11fb2285f90e1acf2fbccd108a5315fded7a
vause@yahoo.com <vause@yahoo.com>
2017-11-28 13:54:08 -0600

    Got each comment instead of root comment
    Iterating through each comment and checking the score of each comment.

--- Comment_Extraction - Count words.py
+++ Comment_Extraction - Count words.py
@@ -1,15 +1,18 @@
 import praw
 from collections import Counter
-list = ['ftfy', 'imo', 'DAE', 'ELI5', 'imho', 'TIL', 'IIRC', 'ITT', 'TLDR', 'TL;DR', 'MIC', 'MFW', 'SMH', 'MRW', 'AFAIK', 'AMA', 'FFS', 'NFSW', 'NSFL', 'TIFU', 'OP', '/s', 'SJW', 'Neckbeard', 'Karmawhore', 'Shitpost', 'Circlejerk' 'rip my inbox']
+
+def words_in_string(word_list, a_string):
+    return set(word_list).intersection(a_string.split())
+
+redditisms = ['ftfy', 'imo', 'DAE', 'ELI5', 'imho', 'TIL', 'IIRC', 'ITT', 'TLDR', 'TL;DR', 'MIC', 'MFW', 'SMH', 'MRW', 'AFAIK', 'AMA', 'FFS', 'NFSW', 'NSFL', 'TIFU', 'OP', '/s', 'SJW', 'Neckbeard', 'Karmawhore', 'Shitpost', 'Circlejerk' 'rip my inbox']
 reddit = praw.Reddit(client_id='BvQLViitNTyU6A',
                      client_secret='IgYF_uDdUYH2Jl8tpY9UFuCf1yY',
                      password='Password1',
 					 user_agent='This will be a script',
                      username='awaythrowcs')
 print (reddit.user.me())
-submission = reddit.submission(url='https://www.reddit.com/r/Jokes/comments/7fukkv/i_wish_my_college_was_run_by_ea/')
+submission = reddit.submission(url='https://www.reddit.com/r/OutOfTheLoop/comments/1yqdkg/what_the_hell_does_ftfy_mean/')
 submission.comments.replace_more(limit=0)
-for top_level_comment in submission.comments:
-    if any(word in top_level_comment.body for word in list):
-        help = Counter(top_level_comment.body.split()).most_common()
-        print(help[:10])
+for sub in submission.comments.list():
+    help1 = sub.body.split()
+    print([sub.score, help1])
