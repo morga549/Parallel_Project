f12fb0175278e10edde0dd0dff66a5cc5abad424
Jack Morgan <jack.art.morgan@gmail.com>
2017-12-03 12:39:04 -0600

    Started on scaper that preprocesses for M/R

---
+++ MR_Preprocess.py
@@ -0,0 +1,39 @@
+import praw
+from praw.models import Comment
+import sys
+import json
+
+outfile = sys.argv[1] # file to print output to
+sub = sys.argv[2] #subreddit to pull from
+
+reddit = praw.Reddit(client_id='BvQLViitNTyU6A',
+                     client_secret='IgYF_uDdUYH2Jl8tpY9UFuCf1yY',
+					 user_agent='This will be a script',
+                    )
+
+sub = reddit.subreddit(sub)
+
+outfile = open(outfile, 'w')
+
+# i = 0
+# for post in sub.hot(limit=20): #every post that we can get from this request
+#     i += 1
+#     if(i % 50 == 0):
+#         print(str(i + 1))
+#     post.comments.replace_more(limit=None) # getting rid of MoreComments objects
+#     for comment in post.comments.list(): # every comment in the flattened comment tree
+#         if isinstance(comment, Comment):
+#             karma = comment.score
+#             output = comment.body.encode('utf-8')
+#             outfile.write(str(karma) + " " + output + "\n")
+
+# **debugging**
+post = reddit.submission(url='https://www.reddit.com/r/redditdev/comments/16a0p8/reddit_comments_list_api_acting_funny/')
+post.comments.replace_more(limit=None)
+for comment in post.comments.list(): # every comment in the flattened comment tree
+    if isinstance(comment, Comment):
+        karma = comment.score
+        output = comment.body.encode('utf-8').replace('\n', '')
+        outfile.write(str(karma) + " " + output + "\n")
+# **debugging**
+outfile.close()
